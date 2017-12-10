69357076a7fa59971fc3ce0c4aad0bdf4244d08c
Jasmine Quach <xjasminequach@gmail.com>
2017-12-10 16:22:25 -0600

    Set style defaults to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class BobbinApp {
   title = 'app';
