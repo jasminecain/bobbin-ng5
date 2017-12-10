a4505c1aeea9ca79d20e861973137507c1ffb37f
Jasmine Quach <xjasminequach@gmail.com>
2017-12-10 16:44:11 -0600

    Generate and include home and login components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BobbinApp } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginComponentComponent } from './component/login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    BobbinApp
+    BobbinApp,
+    HomePageComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule
